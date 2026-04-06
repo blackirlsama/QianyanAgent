d408eaf209f490b190c3850c2e70bf84b979401a
orange <799243133@qq.com>
2026-04-06 20:21:08 +0800

    基于 Redis 实现会话记忆 引入 Embedding 模型 引入 PgVector 向量数据库

--- src/main/java/com/shanyangcode/infintechatagent/ai/AiChatService.java
+++ src/main/java/com/shanyangcode/infintechatagent/ai/AiChatService.java
@@ -2,12 +2,14 @@ package com.shanyangcode.infintechatagent.ai;
 
 import com.shanyangcode.infintechatagent.config.McpToolConfig;
 import com.shanyangcode.infintechatagent.tool.TimeTool;
+import dev.langchain4j.community.store.memory.chat.redis.RedisChatMemoryStore;
 import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.mcp.McpToolProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.rag.content.retriever.ContentRetriever;
 import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
 import dev.langchain4j.service.AiServices;
 import dev.langchain4j.store.embedding.EmbeddingStore;
@@ -25,21 +27,26 @@ public class AiChatService {
     private ChatModel chatModel;
 
     @Resource
-    private EmbeddingStore<TextSegment> embeddingStore;
+    private McpToolProvider mcpToolProvider;
 
     @Resource
-    private McpToolProvider mcpToolProvider;
+    private RedisChatMemoryStore redisChatMemoryStore;
+
+    @Resource
+    private ContentRetriever contentRetriever;
 
     @Bean
     public AiChat aiChat() {
 
-        List<Document> documents = FileSystemDocumentLoader.loadDocuments("src/main/resources/docs");
-        EmbeddingStoreIngestor.ingest(documents, embeddingStore);
-
         return AiServices.builder(AiChat.class)
                 .chatModel(chatModel)
-                .contentRetriever(EmbeddingStoreContentRetriever.from(embeddingStore))
-                .chatMemoryProvider(memoryId -> MessageWindowChatMemory.withMaxMessages(10))
+                .contentRetriever(contentRetriever)
+                .chatMemoryProvider(memoryId -> MessageWindowChatMemory
+                        .builder()
+                        .id(memoryId)
+                        .chatMemoryStore(redisChatMemoryStore)
+                        .maxMessages(20)
+                        .build())
                 .tools(new TimeTool())
                 .toolProvider(mcpToolProvider)
                 .build();
